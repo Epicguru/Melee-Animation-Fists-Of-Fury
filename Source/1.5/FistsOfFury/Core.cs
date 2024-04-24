46cfd794e5f91368cfe0148b77ef677f950482d7
James <james@epicguru.co.uk>
2024-04-24 22:47:01 +0200

    Base mod.

---
+++ Source/1.5/FistsOfFury/Core.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using Verse;
+
+namespace AM.FoF;
+
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
+public sealed class Core : Mod
+{
+    internal static void Log(string msg)
+    {
+        Verse.Log.Message($"<color=cyan>[MA:FoF]</color> {msg}");
+    }
+    
+    public Core(ModContentPack content) : base(content)
+    {
+        Log("Hello, world!");
+    }
+}
