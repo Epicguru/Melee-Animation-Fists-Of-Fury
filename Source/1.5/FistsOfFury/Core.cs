2cc24102fc7cb1c2bc405daaf185cdbbf54f5ead
James Billy <james@epicguru.co.uk>
2024-04-28 00:37:43 +0200

    Fix to target new folder structure of Melee Animation.

--- Source/1.5/FistsOfFury/Core.cs
+++ Source/1.5/FistsOfFury/Core.cs
@@ -1,14 +1,30 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using JetBrains.Annotations;
 using Verse;
 
 namespace AM.FoF;
 
+[HotSwapAll]
 [UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
 public sealed class Core : Mod
 {
+    internal const string DEBUG_ACTION_CATEGORY = "Melee Animation: Fists of Fury";
+    
     internal static void Log(string msg)
     {
         Verse.Log.Message($"<color=cyan>[MA:FoF]</color> {msg}");
+    }
+    
+    internal static void Warn(string msg)
+    {
+        Verse.Log.Warning($"<color=cyan>[MA:FoF]</color> {msg}");
+    }
+    
+    internal static void Error(string msg, Exception e = null)
+    {
+        Verse.Log.Error($"<color=cyan>[MA:FoF]</color> {msg}");
+        if (e != null)
+            Verse.Log.Error(e.ToString());
     }
     
     public Core(ModContentPack content) : base(content)
